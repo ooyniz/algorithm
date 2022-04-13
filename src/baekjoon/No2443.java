cec63ada90831dea4ae9b00270474b5bc862b80b
ooyniz <ooyniz@gmail.com>
2022-04-13 19:54:00 +0900

    First Work

--- src/baekjoon/No2443.java
+++ src/baekjoon/No2443.java
@@ -10,7 +10,7 @@ public class No2443 {
 		for (int i = 0; i < n; i++) {
 			for (int j = 1; j <= i; j++)
 				System.out.print(" ");
-			for (int j = 1; j < 2*(n-i); j++)
+			for (int j = 1; j < 2 * (n - i); j++)
 				System.out.print("*");
 			System.out.println("");
 		}
