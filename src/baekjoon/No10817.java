5256b4f2f3bcd9e30e64c790c6edb09d71f363eb
ooyniz <ooyniz@gmail.com>
2022-04-15 10:17:35 +0900

    Unstaged

--- src/baekjoon/No10817.java
+++ src/baekjoon/No10817.java
@@ -7,11 +7,11 @@ public class No10817 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] list = new int[3];
+		sc.close();
 		for (int i = 0; i < 3; i++)
 			list[i] = sc.nextInt();
 		Arrays.sort(list);
 		System.out.println(list[1]);
-		sc.close();
 	}
 
 }
